65bd54c51b66fb028dce49e55c4b9f14a53a1a9f
Mateo Upegui Borja <upeguiborja@gmail.com>
2020-02-04 17:11:00 -0500

    chore: Added missing files from last commit

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='hello-world',  # Required
+    name='hello_world',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -65,12 +65,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',  # Required
+    version='0.1.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Python packages hello-world',  # Optional
+    description='Python hello-world package',  # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
