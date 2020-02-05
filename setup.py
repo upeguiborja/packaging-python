0b036652396fb9a3a72a1eaa75039c0fb7a5bc3b
Mateo Upegui Borja <upeguiborja@gmail.com>
2020-02-05 09:26:14 -0500

    fix: Changed package name to allow upload to test.pypi.org

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='hello_world',  # Required
+    name='hello_world_upeguiborja',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
